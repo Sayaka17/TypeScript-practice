325b57b1911bbf9aaecb8e6cd770db5b52c27906
kanai_sayaka <kanai_sayaka@benic.co.jp>
2022-08-01 15:19:27 +0900

    信号機の見た目

--- TS-APP/practice01/src/App.tsx
+++ TS-APP/practice01/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import blue from './assets/img/blue_signal.png';
+import yellow from './assets/img/yellow_signal.png';
+import red from './assets/img/red_signal.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>信号機アプリ</h1>
       </header>
+      <div className="signal-wrap">
+        <div className="signal_flex">
+          <div className="signal-box"><img src={blue} /></div>
+          <div className="signal-box"><img src={yellow} /></div>
+          <div className="signal-box"><img src={red} /></div>
+        </div>
+      </div>
+
     </div>
   );
 }
